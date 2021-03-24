ad8fd16f40f25b5922f63227f2dd0152e338a3c7
Mridul <72863438+Kashyap-mridul99@users.noreply.github.com>
2021-03-24 15:27:12 +0530

    Add files via upload

---
+++ Mini Project/js/index.js
@@ -0,0 +1,8 @@
+function myFunc() {
+  var name = document.getElementById("name");
+  var mob = document.getElementById("mob");
+  var email = document.getElementById("email");
+  document.getElementById("outn").innerHTML = name;
+  document.getElementById("outm").innerHTML = mob;
+  document.getElementById("oute").innerHTML = email;
+}
